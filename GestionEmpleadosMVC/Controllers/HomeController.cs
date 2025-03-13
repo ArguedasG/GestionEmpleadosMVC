035c675ba7c1110b7d66280d5975b56839a90cdc
0stylebender0 <120053753+0stylebender0@users.noreply.github.com>
2025-03-13 15:55:13 -0600

    Eliminación de la prueba de conexión.

--- GestionEmpleadosMVC/Controllers/HomeController.cs
+++ GestionEmpleadosMVC/Controllers/HomeController.cs
@@ -7,28 +7,8 @@ namespace GestionEmpleadosMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly String ConnectionString;
-
-        public HomeController(IConfiguration configuration)
-        {
-            ConnectionString = configuration.GetConnectionString("DefaultConnection");
-        }
-
         public IActionResult Index()
         {
-            try
-            {
-                using (SqlConnection cnn = new SqlConnection(ConnectionString))
-                {
-                    cnn.Open();
-                    ViewBag.Mensaje = "Conexión exitosa!";
-                    cnn.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Error = "Error al conectar: " + ex.Message;
-            }
             return View();
         }
 
