700edcca4f6e76662c3420c4f3ba529d1f3eb5cb
0stylebender0 <120053753+0stylebender0@users.noreply.github.com>
2025-03-13 15:34:43 -0600

    Creación del modelo Empleado.

---
+++ GestionEmpleadosMVC/Models/EmpleadoModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlTypes;
+
+namespace GestionEmpleadosMVC.Models
+{
+    public class EmpleadoModel
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public SqlMoney Salario { get; set; }
+    }
+}
