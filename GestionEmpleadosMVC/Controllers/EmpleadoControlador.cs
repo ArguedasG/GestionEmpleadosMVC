aa0ca3b4ca1b0ab81cc1b51606535b59026a396a
0stylebender0 <120053753+0stylebender0@users.noreply.github.com>
2025-03-13 15:42:47 -0600

    Creación del controlador Empleado.

---
+++ GestionEmpleadosMVC/Controllers/EmpleadoControlador.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionEmpleadosMVC.Controllers
+{
+    public class EmpleadoControlador : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
